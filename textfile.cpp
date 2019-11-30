9a9c9e519cab45548469bbb10e7edb2828d269bf
Sayadi <muhammed.alsayadi@gmail.com>
2019-11-30 17:46:08 +0400

    Code style changes in textfile.cpp|

--- textfile.cpp
+++ textfile.cpp
@@ -1,58 +1,52 @@
-// textfile.cpp
-//
-// simple reading and writing for text files
-//
-// www.lighthouse3d.com
-//
-// You may use these functions freely.
-// they are provided as is, and no warranties, either implicit,
-// or explicit are given
-//////////////////////////////////////////////////////////////////////
-
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 char *textFileRead(const char *fn) {
 
 
 	FILE *fp;
-	char *content = NULL;
+	char *content = nullptr;
 
 	int count=0;
 
-	if (fn != NULL) {
+	if (fn != nullptr)
+	{
 		fp = fopen(fn,"rt");
 
-		if (fp != NULL) {
+		if (fp != nullptr)
+    {
       
-      fseek(fp, 0, SEEK_END);
-      count = ftell(fp);
-      rewind(fp);
+        fseek(fp, 0, SEEK_END);
+        count = ftell(fp);
+        rewind(fp);
 
-			if (count > 0) {
-				content = (char *)malloc(sizeof(char) * (count+1));
-				count = fread(content,sizeof(char),count,fp);
-				content[count] = '\0';
-			}
+        if (count > 0)
+        {
+            content = (char *)malloc(sizeof(char) * (count+1));
+            count = fread(content,sizeof(char),count,fp);
+            content[count] = '\0';
+        }
 			fclose(fp);
 		}
 	}
 	return content;
 }
 
-int textFileWrite(char *fn, char *s) {
+int textFileWrite(char *fn, char *s)
+{
 
 	FILE *fp;
 	int status = 0;
 
-	if (fn != NULL) {
+	if (fn != nullptr)
+	{
 		fp = fopen(fn,"w");
 
-		if (fp != NULL) {
+		if (fp != nullptr)
+		{
 			
 			if (fwrite(s,sizeof(char),strlen(s),fp) == strlen(s))
 				status = 1;
@@ -61,10 +55,3 @@ int textFileWrite(char *fn, char *s) {
 	}
 	return(status);
 }
-
-
-
-
-
-
-
